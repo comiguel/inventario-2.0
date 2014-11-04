2c95cc0626ee6828de04c7523a2134fc65d12ce5
Oswaldo De la Ossa <oswaldo244@gmail.com>
2014-11-04 17:05:56 -0500

    nav-bar main done

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => 'Elecsis',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
@@ -36,8 +36,20 @@ AppAsset::register($this);
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
                     ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    ['label' => 'Contact', 'url' => ['/site/contact']],
+                    ['label' => 'Dispositivos', 'url' => ['/dispositivos/']],
+                    ['label' => 'Sims', 'url' => ['/sims/']],
+                    ['label' => 'Proveedores', 'url' => ['/proveedores/']],
+                    ['label' => 'Clientes', 'url' => ['/clientes/']],
+                    ['label' => 'Usuarios', 'url' => ['/clientes/']],
+                    [
+                       'label' => 'Otras opciones',
+                       'items' => [
+                             ['label' => 'Estados', 'url' => ['/estados/']],
+                             ['label' => 'Planes', 'url' => ['/planes/']],
+                             ['label' => 'Contactos', 'url' => ['/contactos/']],
+                             ['label' => 'Historico', 'url' => ['#']],
+                         ],
+                     ],
                     Yii::$app->user->isGuest ?
                         ['label' => 'Login', 'url' => ['/site/login']] :
                         ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
@@ -58,7 +70,7 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+            <p class="pull-left">&copy; Elecsis <?= date('Y') ?></p>
             <p class="pull-right"><?= Yii::powered() ?></p>
         </div>
     </footer>
