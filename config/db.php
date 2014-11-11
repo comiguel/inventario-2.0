ac6f450d9b72898269910618cd1069a6cf0ca393
Oswaldo De la Ossa <oswaldo244@gmail.com>
2014-11-11 11:48:26 -0500

    nueva bd

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=inventario',
+    'dsn' => 'mysql:host=localhost;dbname=inventario2',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
