4fccc74c6f9c7f5cca22fd10f965af0f0d23deae
Carlos Hincapié <carmihinfer@gmail.com>
2014-11-24 09:31:15 -0500

    deploying app

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=inventario2',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=heroku_aff16b3adb61402',
+    'username' => 'b31c08c33848f3',
+    'password' => '8ce88dcc',
     'charset' => 'utf8',
 ];
